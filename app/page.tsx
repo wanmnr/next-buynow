c4f149dff4f0d834fb9d71695b3c8b161d26fb66
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-11-21 23:13:33 +0800

    refactor: relocate files to align with standard Next.js project structure
    Moved files from `src/app` to `app` as most Next.js projects do not use `src` for organization:
    - `src/app/favicon.ico` → `app/favicon.ico`
    - `src/app/globals.css` → `app/globals.css`
    - `src/app/font/GeistMonoVF.woff` → `app/font/GeistMonoVF.woff`
    - `src/app/font/GeistVF.woff` → `app/font/GeistVF.woff`
    - `src/app/page.tsx` → `app/page.tsx`
    - `src/app/layout.tsx` → `app/layout.tsx`
    
    This adjustment adheres to common Next.js conventions and simplifies the directory structure.

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+// app/page.tsx
+// import Image from "next/image";
+import Hero from "./components/home/Hero";
+import CompanyIntro from "./components/home/CompanyIntro";
+import FeaturedProducts from "./components/home/FeaturedProducts";
+import Testimonials from "./components/home/Testimonials";
+import Benefits from "./components/home/Benefits";
+import CallToAction from "./components/home/CallToAction";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <CompanyIntro />
+      <FeaturedProducts />
+      <Benefits />
+      <Testimonials />
+      <CallToAction />
+    </>
+  );
+}
