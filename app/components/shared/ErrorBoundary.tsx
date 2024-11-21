6229840687626376b8627a3c536f3bf11ed24dad
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-11-22 06:59:35 +0800

    feat(ErrorBoundary): display error message in development mode
    Enhanced the ErrorBoundary component to include the display of error messages when `NODE_ENV` is set to "development". Updated state to include the error instance and adjusted rendering logic accordingly.

--- app/components/shared/ErrorBoundary.tsx
+++ app/components/shared/ErrorBoundary.tsx
@@ -8,6 +8,7 @@ interface Props {
 
 interface State {
   hasError: boolean;
+  error?: Error;
 }
 
 export default class ErrorBoundary extends Component<Props, State> {
@@ -16,8 +17,8 @@ export default class ErrorBoundary extends Component<Props, State> {
   };
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public static getDerivedStateFromError(_: Error): State {
-    return { hasError: true };
+  public static getDerivedStateFromError(error: Error): State {
+    return { hasError: true, error };
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
@@ -32,6 +33,9 @@ export default class ErrorBoundary extends Component<Props, State> {
             <h2 className="text-2xl font-bold mb-4">
               Oops, something went wrong!
             </h2>
+            {process.env.NODE_ENV === "development" && (
+              <p className="text-red-600 mb-4">{this.state.error?.message}</p>
+            )}
             <button
               className="bg-blue-600 text-white px-6 py-3 rounded-lg"
               onClick={() => this.setState({ hasError: false })}
